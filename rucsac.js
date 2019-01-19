e48d7e2bbde85189dec03da5897d98c87e0abc42
Adrian <adriandinca2008@yahoo.com>
2019-01-19 14:07:30 +0200

    style: Add dp version (not working)

--- rucsac.js
+++ rucsac.js
@@ -37,3 +37,23 @@ let obiecte = [
     }
   }
 })();
+
+knapSack(W, wt[], val[], n)
+{
+  let i, w;
+  let K[n + 1][W + 1];
+
+  // Build table K[][] in bottom up manner 
+  for (i = 0; i <= n; i++) {
+    for (w = 0; w <= W; w++) {
+      if (i == 0 || w == 0)
+        K[i][w] = 0;
+      else if (wt[i - 1] <= w)
+        K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
+      else
+        K[i][w] = K[i - 1][w];
+    }
+  }
+
+  return K[n][W];
+} 
